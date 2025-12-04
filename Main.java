f158a81925f26243e61ab885bbb1cb8d0295d4c4
Carlos <carlosfvargas526@gmail.com>
2025-12-03 20:53:31 -0800

    clean up comments

--- Main.java
+++ Main.java
@@ -6,10 +6,8 @@
  * ------------------------------------------------------------
  * File: Main.java
  * Purpose: This is the entry point of the program. 
- * I utilize the DijkstrasClass class in order to demonstrate 
+ * I utilize the FloydsClass in order to demonstrate 
  * computing optimal post-to-post costs on a river graph using 
- * Dijkstra's shortest-path algorithm and to allow interactive 
- * testing of routes between posts. 
  **************************************************************/
 
 
@@ -20,11 +18,8 @@ import java.util.Scanner;
 public class Main {
     /*********************************************************
      * Function main; run this function to execute the river
-     * routing program. The method reads an optional input
-     * filename from the command line, constructs a 
-     * DijkstrasClass instance using that file, prints the 
-     * optimal cost matrix, and then enters an interactive 
-     * loop where the user can test post-to-post routes.
+     * routing program. Essentially turning a graph class 
+     * into a class appropriate for this problem.
      * 
      * @param args optional command-line arguments; if args[0]
      *             is present, it is used as the input filename
@@ -38,10 +33,10 @@ public class Main {
 
         // DijkstrasClass riverInstance = new DijkstrasClass(filename);
         FloydsClass riverInstance = new FloydsClass(filename);
-        // riverInstance.printShortestPaths();
+        riverInstance.displayResults();
         
         // Now let the user test routes between posts 
-        // testRoutes(riverInstance);
+        testRoutes(riverInstance);
         
         System.out.println("\n-------------------------------------------------");
     }
@@ -51,40 +46,38 @@ public class Main {
      * Function testRoutes; repeatedly prompts the user to 
      * test optimal routes between pairs of posts. When the 
      * user chooses to continue, they enter a pair of post 
-     * indices (u and v), and this function calls 
-     * printOptimalRouteInfo on the provided DijkstrasClass 
-     * instance to display the optimal distance and path.
-     * Input is validated, and invalid entries are reported.
+     * indices (u and v), and this function displays the 
+     * optimal distance and path.
      * 
-     * @param riverInstance a DijkstrasClass object that has 
+     * @param riverInstance a FloydsClass object that has 
      *        already computed all shortest-path information
      *********************************************************/
-    // static void testRoutes(FloydsClass riverInstance) { 
-    //     int u = -1, v = -1;
-    //     boolean contin = true; 
-    //     Scanner sc = new Scanner(System.in);
+    static void testRoutes(FloydsClass riverInstance) { 
+        int u = -1, v = -1;
+        boolean contin = true; 
+        Scanner sc = new Scanner(System.in);
 
-    //     while (contin) { 
-    //         System.out.print("\nTest another post-to-post cost (y/n)? ");
-    //         String line = sc.nextLine(); 
+        while (contin) { 
+            System.out.print("\nTest another post-to-post cost (y/n)? ");
+            String line = sc.nextLine(); 
 
-    //         if ("n".equals(line.toLowerCase())) {
-    //             contin = false;
-    //         }
+            if ("n".equals(line.toLowerCase())) {
+                contin = false;
+            }
 
-    //         else if ("y".equals(line.toLowerCase())) {
-    //             try { 
-    //                 System.out.print("Choose the from and to posts, seperated by a space: ");
-    //                 line = sc.nextLine(); 
-    //                 String[] uv = line.trim().split("\\s+");
-    //                 u = Integer.parseInt(uv[0]);
-    //                 v = Integer.parseInt(uv[1]);
+            else if ("y".equals(line.toLowerCase())) {
+                try { 
+                    System.out.print("Choose the from and to posts, seperated by a space: ");
+                    line = sc.nextLine(); 
+                    String[] uv = line.trim().split("\\s+");
+                    u = Integer.parseInt(uv[0]);
+                    v = Integer.parseInt(uv[1]);
                     
-    //                 // riverInstance.printOptimalRouteInfo(u, v);
-    //             } catch (Exception e) { 
-    //                 System.out.println("Invalid post input: " + e);
-    //             }
-    //         }
-    //     }
-    // }
+                    riverInstance.displayPath(u, v);
+                } catch (Exception e) { 
+                    System.out.println("Invalid post input: " + e);
+                }
+            }
+        }
+    }
 }
